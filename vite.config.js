2931fdead7db62cd6230e48a1d6b2c1cd2993dfa
ibrahim elm <ibrahimelmailoudi@gmail.com>
2024-09-22 20:03:01 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base:"/Portfolio-website/",
   server: {
     // Ensure that Vite serves index.html for any non-file requests
     fs: {
